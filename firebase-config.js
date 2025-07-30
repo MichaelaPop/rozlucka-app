20cce4c9c0a3033a6330873e801a7850abd30831
MichaelaPop <mesha.spoluprace@gmail.com>
2025-07-30 05:51:37 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-// Tvoje Firebase konfigurace
 const firebaseConfig = {
   apiKey: "AIzaSyCweM-p_XTzZlgMX89xDW2umG0pxfo3WRQ",
   authDomain: "tinky-rozlucka.firebaseapp.com",
@@ -11,7 +7,3 @@ const firebaseConfig = {
   appId: "1:756403512507:web:53c0af432b78e925b1b59e",
   measurementId: "G-XNQRVV2KW5"
 };
-
-// Inicializace Firebase
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
