27aac0688ea8206b1f7946c71e02d818efc1ca61
MichaelaPop <mesha.spoluprace@gmail.com>
2025-07-30 12:35:38 +0200

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Tvoje Firebase konfigurace
 const firebaseConfig = {
-  apiKey: "Tvoje API key",
-  authDomain: "Tvoje authDomain",
-  projectId: "Tvoje projectId",
-  storageBucket: "Tvoje storageBucket",
-  messagingSenderId: "Tvoje messagingSenderId",
-  appId: "Tvoje appId",
-  measurementId: "Tvoje measurementId"
+  apiKey: "AIzaSyCweM-p_XTzZlgMX89xDW2umG0pxfo3WRQ",
+  authDomain: "tinky-rozlucka.firebaseapp.com",
+  projectId: "tinky-rozlucka",
+  storageBucket: "tinky-rozlucka.firebasestorage.app",
+  messagingSenderId: "756403512507",
+  appId: "1:756403512507:web:53c0af432b78e925b1b59e",
+  measurementId: "G-XNQRVV2KW5"
 };
 
+// Inicializace Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
