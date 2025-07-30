23cd38e9327502ff484d7e7010c5c81782e3ff72
MichaelaPop <mesha.spoluprace@gmail.com>
2025-07-30 03:05:18 -0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+/*
+ * Konfigurační soubor Firebase.
+ *
+ * Tento soubor slouží k inicializaci Firebase aplikace. Po vytvoření
+ * projektu ve Firebase Console sem doplňte příslušné hodnoty. Bez správné
+ * konfigurace nebude ukládání a načítání skóre fungovat.
+ */
+
+// TODO: vyplňte vlastní konfiguraci podle instrukcí v README.md
+const firebaseConfig = {
+  apiKey: "VLOZTE_API_KEY",
+  authDomain: "VLOZTE_AUTH_DOMAIN",
+  projectId: "VLOZTE_PROJECT_ID",
+  storageBucket: "VLOZTE_STORAGE_BUCKET",
+  messagingSenderId: "VLOZTE_MESSAGING_SENDER_ID",
+  appId: "VLOZTE_APP_ID"
+};
